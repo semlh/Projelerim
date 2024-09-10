2074d160c0237d9f5c4197ad1910dd76a7ba5471
semlh <semih2626eski@gmail.com>
2024-09-10 15:37:20 +0300

    10.09.2024-2

--- index3.js
+++ index3.js
@@ -1,46 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('emailFormm');
-    
-    if (form) {
-        form.addEventListener('submit', function(event) {
-            event.preventDefault(); // Sayfanın yeniden yüklenmesini engeller
-
-            // Form elemanlarını al
-            const nameElement = document.getElementById('name');
-            const emailElement = document.getElementById('email');
-            const phoneElement = document.getElementById('phone');
-            const messageElement = document.getElementById('message');
-            
-            // Değişkenlere atama
-            const name = nameElement ? nameElement.value : '';
-            const email = emailElement ? emailElement.value : '';
-            const phone = phoneElement ? phoneElement.value : '';
-            const messageContent = messageElement ? messageElement.value : '';
-
-            // Form doğrulaması
-            if (!name || !email || !phone || !messageContent) {
-                alert('Lütfen tüm alanları doldurun.');
-                return;
-            }
-
-            // E-posta formatı kontrolü
-            const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-            if (!emailPattern.test(email)) {
-                alert('Geçerli bir e-posta adresi girin.');
-                return;
-            }
-
-            // E-posta konusunu ve içeriğini ayarlama
-            const subject = 'Yeni İletişim Formu Mesajı';
-            const body = Ad: ${name}\nEmail: ${email}\nTelefon: ${phone}\nMesaj: ${messageContent};
-
-            // mailto protokolü ile e-posta oluşturma
-            const mailtoLink = mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)};
-
-            // E-posta istemcisinde yeni e-posta açma
-            window.location.href = mailtoLink;
-        });
-    } else {
-        console.error('Form bulunamadı.');
-    }
+const themeToggleBtn = document.getElementById('theme-toggle');
+themeToggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
 });
+
+const matchDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+function applyThemeChange(event) {
+  if (event.matches) {
+    document.body.classList.add('dark-theme');
+  } else {
+    document.body.classList.remove('dark-theme');
+  }
+}
+
+matchDarkMode.addListener(applyThemeChange);
+applyThemeChange(matchDarkMode);
