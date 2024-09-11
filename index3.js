5580c61fcfd718532ac8abb5f5e6c3cfb2e759fe
semlh <semih2626eski@gmail.com>
2024-09-11 11:39:08 +0300

    bozuk

--- index3.js
+++ index3.js
@@ -1,8 +1,13 @@
 const button=document.getElementById('darkmode');
 const body= document.body
+const navs= document.getElementById('darkmodnav');
+
 
 button.addEventListener('click',()=>
     {
+        
         body.classList.toggle('darkmod')
+        nav.classList.toggle('darkmod1')
     });
 
+ 
