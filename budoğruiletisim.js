f092ec382fc70a8af555bdbb5afd6d6693374baf
semlh <semih2626eski@gmail.com>
2024-09-11 15:31:45 +0300

    dark mod bitti

--- budoğruiletisim.js
+++ budoğruiletisim.js
@@ -3,18 +3,16 @@ const body= document.body
 const navs= document.getElementById('darkmodnav');
 const button1=document.getElementById('anamenubtn');
 const ayuak=document.getElementById('footers');
-
+const iletism=document.getElementById('emailFormm');
 
 
 
 button.addEventListener('click',()=>
     {
-        
+        iletism.classList.toggle('ilet')
         ayuak.classList.toggle('foot')
         button.classList.toggle('darkmodel');
         button1.classList.toggle('darkmode2')
         body.classList.toggle('darkmods')
         navs.classList.toggle('darkmod1')
     });
-
- 
--- iletisim.js
+++ iletisim.js
@@ -3,6 +3,10 @@ const body= document.body
 const navs= document.getElementById('darkmodnav');
 const button1=document.getElementById('anamenubtn');
 const ayuak=document.getElementById('footers');
+const yazıb=document.getElementById('yazı');
+const yazıb2=document.getElementById('yazı2');
+const yazıb3=document.getElementById('yazı3');
+const yazıb4=document.getElementById('yazı4');
 
 
 
@@ -10,11 +14,13 @@ const ayuak=document.getElementById('footers');
 button.addEventListener('click',()=>
     {
         
+        yazıb.classList.toggle('yazıcss')
+        yazıb2.classList.toggle('yazıcss1')
+        yazıb3.classList.toggle('yazıcss2')
+        yazıb4.classList.toggle('yazıcss3')
         ayuak.classList.toggle('foot')
         button.classList.toggle('darkmodel');
         button1.classList.toggle('darkmode2')
         body.classList.toggle('darkmods')
         navs.classList.toggle('darkmod1')
     });
-
- 
