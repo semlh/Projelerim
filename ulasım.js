ad490c22b8ea28a5a9c70382de71c200b86182c0
semlh <semih2626eski@gmail.com>
2024-09-16 16:08:13 +0300

    deneyimler kısmı bitti

--- ulasım.js
+++ ulasım.js
@@ -3,14 +3,17 @@ const body= document.body
 const navs= document.getElementById('darkmodnav');
 const button1=document.getElementById('anamenubtn');
 const ayuak=document.getElementById('footers');
+const ulyaz=document.getElementById('yazıssd');
+
+
+
 
 button.addEventListener('click',()=>
     {
+        ulyaz.classList.toggle('ullyaz')
         ayuak.classList.toggle('foot')
         button.classList.toggle('darkmodel')
         button1.classList.toggle('darkmode2')
         body.classList.toggle('darkmods')
         navs.classList.toggle('darkmod1')
     });
-
- 
